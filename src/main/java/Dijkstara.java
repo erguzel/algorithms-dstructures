0d9eb74fe6ad5d93d173ce460c11bfa248c90685
sicsrobotics <admin@olgunerguzel.com>
2022-02-20 16:59:46 +0100

    Djkstara shortest path improve

--- src/main/java/Dijkstara.java
+++ src/main/java/Dijkstara.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Dijkstara {
 
+    ALogger<Dijkstara> LOGGER = new ALogger<>(Dijkstara.class);
+
     private static class MinMinDistanceTo {
 
-        public int id = -1;
+      public int id = -1;
         public MinMinDistanceTo from;
         public double minDist = Integer.MAX_VALUE;
         public int previous = -1;
-
-
     }
 
 
@@ -82,7 +82,7 @@ public class Dijkstara {
 
     public double getShortestPath(int[][][] paramGraph, int paramSourceId, int paramDestId) {
 
-        long startTime = System.currentTimeMillis();
+        Throwable stackTrace = new Throwable();
 
 
         boolean isValidGraph = paramGraph == null ? false : paramGraph.length == 0 ? false : true;
@@ -102,79 +102,75 @@ public class Dijkstara {
 
 
         boolean[] visited = new boolean[paramGraph.length];
-       // Queue<MinMinDistanceTo> priorityQueue = new LinkedList<>();
         Queue<MinMinDistanceTo> priorityQueue = new PriorityQueue<MinMinDistanceTo>((a, b) -> a.minDist > b.minDist ? 1 : -1);
-        MinMinDistanceTo localExtremum = results[paramSourceId];
-        localExtremum.id = paramSourceId;
-        localExtremum.minDist = 0;
-        priorityQueue.add(localExtremum);
-        results[paramSourceId] = localExtremum;
-        visited[paramSourceId] = true;
+        MinMinDistanceTo startPoint = results[paramSourceId];
+        startPoint.id = paramSourceId;
+        startPoint.minDist = 0;
+        priorityQueue.add(startPoint);
+        results[paramSourceId] = startPoint;
+        List<String> trajectory = new ArrayList<>();
 
 
-        double distanceSoFar = 0;
+        double currentPointdistance = 0;
 
         int popCount = 0;
 
         while (!priorityQueue.isEmpty()) {
 
-            MinMinDistanceTo nextLocalExtremum = priorityQueue.poll();
+            MinMinDistanceTo currentPoint = priorityQueue.poll();
 
-//            if (visited[nextLocalExtremum.id]) continue;
-//            visited[nextLocalExtremum.id] = true;
+            trajectory.add(currentPoint.previous+"->"+currentPoint.id+"="+currentPoint.minDist);
 
-          //  System.out.println("Traversing->" + nextLocalExtremum.id);
-            popCount++;
+           popCount++;
 
-            distanceSoFar = results[nextLocalExtremum.id].minDist;
+            currentPointdistance = results[currentPoint.id].minDist;
 
-            for (int i = 0; i < paramGraph[nextLocalExtremum.id].length; i++) {
+            for (int i = 0; i < paramGraph[currentPoint.id].length; i++) {
 
-                int neighbourIndex = paramGraph[nextLocalExtremum.id][i][0];
-            //    if(visited[neighbourIndex])continue;
+                int neighbourIndex = paramGraph[currentPoint.id][i][0];
 
-                double weight = paramGraph[nextLocalExtremum.id][i][1];
-                double newDist = distanceSoFar + weight;
+                double weight = paramGraph[currentPoint.id][i][1];
+                double newDist = currentPointdistance + weight;
+
                 if (newDist < results[neighbourIndex].minDist) {
                     results[neighbourIndex].minDist = newDist;
                     results[neighbourIndex].id = neighbourIndex;
-                    results[neighbourIndex].previous = nextLocalExtremum.id;
-                    if(!visited[neighbourIndex]){
-                        visited[neighbourIndex] = true;
-                        priorityQueue.add(results[neighbourIndex]);
-                    }
+                    results[neighbourIndex].previous = currentPoint.id;
+                    priorityQueue.add(results[neighbourIndex]);
+                    priorityQueue.remove(currentPoint);
+//                    if (!visited[neighbourIndex]) {
+//                        visited[neighbourIndex] = true;
+//                        priorityQueue.add(results[neighbourIndex]);
+//                    }
                 }
             }
 
         }
-        System.out.println("PollCount: " + popCount);
+        LOGGER.info("POLLCOUNT="+popCount,stackTrace);
+
 
         int i = paramDestId;
         List path = new ArrayList();
         while (results[i].previous != -1) {
 
 
-
-                path.add(results[i].previous);
-                i = results[i].previous;
+            path.add(results[i].previous);
+            i = results[i].previous;
 
 
         }
 
 
+
 //        Arrays.stream(results).forEach(a -> System.out.println(paramSourceId + "->" + a.id + ":" + a.minDist));
 
 
-        System.out.println("------------");
         Collections.reverse(path);
-        System.out.println(path);
+        LOGGER.info("PATH:"+path,stackTrace);
+        LOGGER.info("TRAJECTORY:"+trajectory,stackTrace);
 
-        System.out.println("---- result---");
-
-        System.out.println(results[paramDestId].minDist);
-
-        long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime - startTime) + "ms");
+        //System.out.println("---- result---");
+        LOGGER.info("Result:"+results[paramDestId].minDist,stackTrace);
 
         return results[paramDestId].minDist;
 
@@ -204,8 +200,9 @@ public class Dijkstara {
         };
 
 
+
         Dijkstara d = new Dijkstara();
-        double res = d.getShortestPath(SampleInputs.get18weightedShortestPath(), 0, 9);
+        double res = d.getShortestPath(SampleInputs.GraphOnlineRu.convertToAdjacencyList(SampleInputs.GraphOnlineRu.GO0001_ADJMTX), 3, 7);
 
 
     }
