ff96f2ea6ffb752cf6b964a3e897c68042c90a9b
erguzel <erguzel@itu.edu.tr>
2022-02-22 21:47:00 +0100

    bfs dfs

--- src/main/java/BFS.java
+++ src/main/java/BFS.java
@@ -1,9 +1,39 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class DFS {
+/**
+ * Use Cases
+ *1) Shortest Path and Minimum Spanning Tree for unweighted graph In an unweighted graph, the shortest path is the path with least number of edges. With Breadth First, we always reach a vertex from given source using the minimum number of edges. Also, in case of unweighted graphs, any spanning tree is Minimum Spanning Tree and we can use either Depth or Breadth first traversal for finding a spanning tree.
+ *
+ * 2) Peer to Peer Networks. In Peer to Peer Networks like BitTorrent, Breadth First Search is used to find all neighbor nodes.
+ *
+ * 3) Crawlers in Search Engines: Crawlers build index using Breadth First. The idea is to start from source page and follow all links from source and keep doing same. Depth First Traversal can also be used for crawlers, but the advantage with Breadth First Traversal is, depth or levels of the built tree can be limited.
+ *
+ * 4) Social Networking Websites: In social networks, we can find people within a given distance ‘k’ from a person using Breadth First Search till ‘k’ levels.
+ *
+ * 5) GPS Navigation systems: Breadth First Search is used to find all neighboring locations.
+ *
+ * 6) Broadcasting in Network: In networks, a broadcasted packet follows Breadth First Search to reach all nodes.
+ *
+ * 7) In Garbage Collection: Breadth First Search is used in copying garbage collection using Cheney’s algorithm. Refer this and for details. Breadth First Search is preferred over Depth First Search because of better locality of reference:
+ *
+ * 8) Cycle detection in undirected graph: In undirected graphs, either Breadth First Search or Depth First Search can be used to detect cycle. We can use BFS to detect cycle in a directed graph also,
+ *
+ * 9) Ford–Fulkerson algorithm In Ford-Fulkerson algorithm, we can either use Breadth First or Depth First Traversal to find the maximum flow. Breadth First Traversal is preferred as it reduces worst case time complexity to O(VE2).
+ *
+ * 10) To test if a graph is Bipartite We can either use Breadth First or Depth First Traversal.
+ *
+ * 11) Path Finding We can either use Breadth First or Depth First Traversal to find if there is a path between two vertices.
+ *
+ * 12) Finding all nodes within one connected component: We can either use Breadth First or Depth First Traversal to find all nodes reachable from a given node.
+ *
+ * Many algorithms like Prim’s Minimum Spanning Tree and Dijkstra’s Single Source Shortest Path use structure similar to Breadth First Search.
+ *
+ * There can be many more applications as Breadth First Search is one of the core algorithms for Graphs.
+ */
+public class BFS {
 
-    ALogger<DFS> LOGGER = new ALogger<>(DFS.class);
+    ALogger<BFS> LOGGER = new ALogger<>(BFS.class);
 
     public static void main(String[] args) {
         int[][][] graph = SampleData.convertToAdjacencyList(SampleData.GraphOnlineRu.DUCK_DIR_WEG);
@@ -12,163 +42,135 @@ public class DFS {
 
         ALogger.TIMER timer = new ALogger.TIMER();
         timer.startTimer();
-        DFS bfsAgain = new DFS();
-        bfsAgain.findPathExists(graph, 9, 2);
+        BFS bfsAgain = new BFS();
+        bfsAgain.findTrajectoryAndShortestPath(graph, 9, 0);
         timer.getBenchmark(timer);
     }
 
-    /**
-     *
-     * @param graph as adj lists
-     * @param paramsourceid
-     * @param paramtargetid
-     * @return
-     */
-    public boolean findPathExists(int[][][] graph, int paramsourceid, int paramtargetid) {
-
-        List<Integer> trajectory = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        stack.add(paramsourceid);
-
-        Object[] previous = new Object[graph.length];
+    public void findTrajectoryAndShortestPath(int[][] graph, int paramsourceid, int paramtargetid){
         boolean[] visited = new boolean[graph.length];
+        Object[] previous = new Object[graph.length]; // default null
+        boolean[] pathExists= new boolean[graph.length];
+        int[] distances = new int[graph.length];
+        List<Integer> trajectory = new ArrayList<>();
+        for(int i = 0; i < distances.length;i++){distances[i] = Integer.MAX_VALUE;}//for defaults
+        distances[paramsourceid] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(paramsourceid);
 
-        int pollcount = 0;
-        boolean pathexsist = false;
-
-        while (!stack.isEmpty()) {
-
-            int current = stack.pop();
-            pollcount++;
-
-            if (visited[current]) {
-                continue;
-            }
-            trajectory.add(current);
+        int pollCount = 0;
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            pollCount++;
+            if(visited[current]){continue;}// if visited
             visited[current] = true;
-
-            for (int i = 0; i < graph[current].length; i++) {
-
-                    int nbid = graph[current][i][0];
-                    int weight = graph[current][i][1]; // weight no of use in DFS
-
-                    if (nbid == paramtargetid) {
-
-                        pathexsist = true;
-                    }
-
-                    if (!visited[nbid]) {
+            trajectory.add(current);
+            int distanceSoFar = distances[current];
+// visit nbours
+            for(int i = 0; i < graph[current].length; i++){
+                if(graph[current][i] != 0){
+                    int nbid = i;
+                    int weight = 1;// default nonweighted graph
+                    int newDistance  =  distanceSoFar + weight;
+                    if(newDistance  < distances[nbid]){
+                        distances[nbid] = newDistance;
                         previous[nbid] = current;
-                        stack.add(nbid);
-                    }
-
-
-            }//for
-        }//while
-
-        if (pathexsist) {
-
-            LOGGER.info("there exists path " + paramsourceid + "->" + paramtargetid);
-
-        }
-
-        LOGGER.info("Tajectory:" + trajectory);
-        LOGGER.info("PopCount:" + pollcount);
+                        pathExists[nbid] = true;
+                        if(!visited[nbid]){
+                            queue.add(nbid);
+                        }//if add queue
+                    }//if compare dist
+                }// skip 0 s
+            }// for nbours
+        }//while queue
 
         int target = paramtargetid;
         List<Object> path = new ArrayList<>();
         path.add(paramtargetid);
-
-        while (previous[target] != null) {
-
+        while(previous[target] != null){
             path.add(previous[target]);
             target = (int) previous[target];
         }
+
+        if (pathExists[paramtargetid]) {
+
+            LOGGER.info("there exists path " + paramsourceid + "->" + paramtargetid);
+
+        }else
+            LOGGER.info("there exists NO path " + paramsourceid + "->" + paramtargetid);
+
+        LOGGER.info("Tajectory:" + trajectory);
+        LOGGER.info("PopCount:" + pollCount);
+        LOGGER.info("Shortest Distance:" + distances[paramtargetid]);
 
         Object[] reversed = path.toArray();
         ReverseArray.reverse(reversed);
 
         LOGGER.info("Path:" + Arrays.stream(reversed).collect(Collectors.toList()));
 
+    }//findTrajectory..
 
-
-        return pathexsist;
-
-    }//findPathExists
-
-    /**
-     *
-     * @param graph as adj mtx
-     * @param paramsourceid
-     * @param paramtargetid
-     * @return
-     */
-    public boolean findPathExists(int[][] graph, int paramsourceid, int paramtargetid) {
-
-        List<Integer> trajectory = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        stack.add(paramsourceid);
-
-        Object[] previous = new Object[graph.length];
+    public void findTrajectoryAndShortestPath(int[][][] graph, int paramsourceid, int paramtargetid){
         boolean[] visited = new boolean[graph.length];
+        Object[] previous = new Object[graph.length]; // default null
+        boolean[] pathExists= new boolean[graph.length];
+        int[] distances = new int[graph.length];
+        List<Integer> trajectory = new ArrayList<>();
+        for(int i = 0; i < distances.length;i++){distances[i] = Integer.MAX_VALUE;}//for defaults
+        distances[paramsourceid] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(paramsourceid);
 
-        int pollcount = 0;
-        boolean pathexsist = false;
-
-        while (!stack.isEmpty()) {
-
-            int current = stack.pop();
-            pollcount++;
-
-            if (visited[current]) {
-                continue;
-            }
-            trajectory.add(current);
+        int pollCount = 0;
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            pollCount++;
+            if(visited[current]){continue;}// if visited
             visited[current] = true;
+            trajectory.add(current);
+            int distanceSoFar = distances[current];
+// visit nbours
+            for(int i = 0; i < graph[current].length; i++){
 
-            for (int i = 0; i < graph[current].length; i++) {
-                if (graph[current][i] != 0) {
-                    int nbid = i;
-
-                    if (nbid == paramtargetid) {
-
-                        pathexsist = true;
-                    }
-
-                    if (!visited[nbid]) {
+                    int nbid = graph[current][i][0];
+                    int weight = 1;// default nonweighted graph
+                    int newDistance  =  distanceSoFar + weight;
+                    if(newDistance  < distances[nbid]){
+                        distances[nbid] = newDistance;
                         previous[nbid] = current;
-                        stack.add(nbid);
-                    }
+                        pathExists[nbid] = true;
+                        if(!visited[nbid]){
+                            queue.add(nbid);
+                        }//if add queue
+                    }//if compare dist
 
-                }
-            }//for
-        }//while
-
-        if (pathexsist) {
-
-            LOGGER.info("there exists path " + paramsourceid + "->" + paramtargetid);
-
-        }
-
-        LOGGER.info("Tajectory:" + trajectory);
-        LOGGER.info("PopCount:" + pollcount);
+            }// for nbours
+        }//while queue
 
         int target = paramtargetid;
         List<Object> path = new ArrayList<>();
         path.add(paramtargetid);
-        while (previous[target] != null) {
-
+        while(previous[target] != null){
             path.add(previous[target]);
             target = (int) previous[target];
         }
 
+        if (pathExists[paramtargetid]) {
+
+            LOGGER.info("there exists path " + paramsourceid + "->" + paramtargetid);
+
+        }else
+            LOGGER.info("there exists NO path " + paramsourceid + "->" + paramtargetid);
+
+        LOGGER.info("Tajectory:" + trajectory);
+        LOGGER.info("PopCount:" + pollCount);
+        LOGGER.info("Shortest Distance:" + distances[paramtargetid]);
+
         Object[] reversed = path.toArray();
         ReverseArray.reverse(reversed);
 
-       LOGGER.info("Path:" + Arrays.stream(reversed).collect(Collectors.toList()));
+        LOGGER.info("Path:" + Arrays.stream(reversed).collect(Collectors.toList()));
 
+    }//findTrajectory..
 
-        return pathexsist;
-
-    }//findPathExists
 }//class
--- src/main/java/DFS.java
+++ src/main/java/DFS.java
@@ -1,23 +1,93 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class DFSAgain {
+public class DFS {
 
-    ALogger<DFSAgain> LOGGER = new ALogger<>(DFSAgain.class);
+    ALogger<DFS> LOGGER = new ALogger<>(DFS.class);
 
     public static void main(String[] args) {
+        // int[][][] graph = SampleData.convertToAdjacencyList(SampleData.GraphOnlineRu.DUCK_DIR_WEG);
         int[][] graph = SampleData.GraphOnlineRu.DUCK_DIR_WEG;
 
         System.out.println();
 
         ALogger.TIMER timer = new ALogger.TIMER();
         timer.startTimer();
-        DFSAgain bfsAgain = new DFSAgain();
-        bfsAgain.findPathExists(graph, 9, 2);
+        DFS df = new DFS();
+        df.findTrajectoryAndPathExists(graph, 9, 2);
         timer.getBenchmark(timer);
     }
 
-    public boolean findPathExists(int[][] graph, int paramsourceid, int paramtargetid) {
+    /**
+     *
+     * @param graph adj mtx
+     * @param paramsourceid
+     * @param paramtargetid
+     */
+    public void findTrajectoryAndPathExists(int[][] graph, int paramsourceid, int paramtargetid) {
+
+        boolean[] visited = new boolean[graph.length];
+        boolean[] pathExists = new boolean[graph.length];
+        Object[] previous = new Object[graph.length];
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(paramsourceid);
+        List<Integer> trajectory = new ArrayList<>();
+        int popcount = 0;
+        while (!stack.isEmpty()) {
+            int currentPoint = stack.pop();
+            popcount++;
+            if (visited[currentPoint]) {
+                continue;
+            }//if visited
+            trajectory.add(currentPoint);
+            visited[currentPoint] = true;
+// explore nbours
+            for (int i = 0; i < graph[currentPoint].length; i++) {
+                if (graph[currentPoint][i] != 0) {
+                    int nbidx = i;
+                    pathExists[nbidx] = true;
+                    if (!visited[nbidx]) {
+                        previous[nbidx] = currentPoint;
+                        stack.add(nbidx);
+                    }// if visited
+                }//if
+            }//for point
+        }//while stack isempty
+
+        int tgt = paramtargetid;
+        List<Object> pathFound = new ArrayList<>();
+        pathFound.add(paramtargetid);
+
+        while (previous[tgt] != null) {
+            pathFound.add(previous[tgt]);
+            tgt = (int) previous[tgt];
+        }//while
+
+        Object[] reversed = pathFound.toArray();
+        ReverseArray.reverse(reversed);
+
+
+        if (pathExists[paramtargetid]) {
+
+            LOGGER.info("there exists path " + paramsourceid + "->" + paramtargetid);
+
+        }
+        LOGGER.info("Tajectory:" + trajectory);
+        LOGGER.info("PopCount:" + popcount);
+        LOGGER.info("Found Path:" + Arrays.stream(reversed).collect(Collectors.toList()));
+
+
+    }//findTrajectoryAndPathExists
+
+
+    /**
+     * @param graph         as adj lists
+     * @param paramsourceid
+     * @param paramtargetid
+     * @return
+     */
+    public boolean findTrajectoryAndPathExists(int[][][] graph, int paramsourceid, int paramtargetid) {
 
         List<Integer> trajectory = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
@@ -25,6 +95,7 @@ public class DFSAgain {
 
         Object[] previous = new Object[graph.length];
         boolean[] visited = new boolean[graph.length];
+        boolean[] pathExists = new boolean[graph.length];
 
         int pollcount = 0;
         boolean pathexsist = false;
@@ -41,24 +112,22 @@ public class DFSAgain {
             visited[current] = true;
 
             for (int i = 0; i < graph[current].length; i++) {
-                if (graph[current][i] != 0) {
-                    int nbid = i;
 
-                    if (nbid == paramtargetid) {
+                int nbid = graph[current][i][0];
+                int weight = graph[current][i][1]; // weight no of use in DFS
 
-                        pathexsist = true;
-                    }
+                pathExists[nbid] = true;
 
-                    if (!visited[nbid]) {
-                        previous[nbid] = current;
-                        stack.add(nbid);
-                    }
-
+                if (!visited[nbid]) {
+                    previous[nbid] = current;
+                    stack.add(nbid);
                 }
+
+
             }//for
         }//while
 
-        if (pathexsist) {
+        if (pathExists[paramtargetid]) {
 
             LOGGER.info("there exists path " + paramsourceid + "->" + paramtargetid);
 
@@ -70,6 +139,7 @@ public class DFSAgain {
         int target = paramtargetid;
         List<Object> path = new ArrayList<>();
         path.add(paramtargetid);
+
         while (previous[target] != null) {
 
             path.add(previous[target]);
@@ -79,7 +149,7 @@ public class DFSAgain {
         Object[] reversed = path.toArray();
         ReverseArray.reverse(reversed);
 
-       LOGGER.info("Path:" + Arrays.stream(reversed).collect(Collectors.toList()));
+        LOGGER.info("Path:" + Arrays.stream(reversed).collect(Collectors.toList()));
 
 
         return pathexsist;
