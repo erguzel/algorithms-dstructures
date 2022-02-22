f40c10842b2552d2fa02661e1fd31e21d1276bba
erguzel <erguzel@itu.edu.tr>
2022-02-22 21:51:14 +0100

    bublesrt, arr reverse

--- src/main/java/ReverseArray.java
+++ src/main/java/ReverseArray.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class ReverseArray {
-}
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.stream.Collectors;
+
+public class ReverseArray {
+
+    static ALogger<ReverseArray> LOGGER = new ALogger<>(ReverseArray.class);
+    public static void reverse(Object [] arr) {
+
+
+        int endIndex = 0;
+        for(int i = 0; i < arr.length ;i++){
+            endIndex = (arr.length-i-1);
+            if(i>=endIndex){
+                return;
+            }
+            Object  temp = arr[i];
+            arr[i] = arr[endIndex];
+            arr[endIndex] = temp;
+
+
+        }//for
+    }//reverse
+
+    public static void main(String[] args) {
+
+        Object [] arr = Arrays.stream(SampleData.generateIntArray(15,-4,12)).mapToObj(a->a).toArray();
+
+        LOGGER.info(Arrays.stream(arr).collect(Collectors.toList()));
+
+        ALogger.TIMER timer = new ALogger.TIMER();
+        timer.startTimer();
+
+        ReverseArray reverseArray = new ReverseArray();
+        reverseArray.reverse(arr);
+        LOGGER.info(Arrays.stream(arr).collect(Collectors.toList()));
+        timer.getBenchmark(timer);
+
+    }
+}//class
