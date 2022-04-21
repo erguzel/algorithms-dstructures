c0252d9f60f0d41b337c9177e44454774ae2d9e8
erguzel <erguzel@itu.edu.tr>
2022-04-21 15:37:20 +0200

    refactor

--- src/main/java/CycleDetectionDirected.java
+++ src/main/java/CycleDetectionDirected.java
@@ -11,40 +11,39 @@ public class CycleDetectionDirected {
 
     //TODO: recursive impl
 
-    public boolean hasCycle(int[][] edgelist,int numberofvertex){
+    public boolean hasCycle(int[][] edgelist, int numberofvertex){
 
         int edgeListLength = edgelist.length;
         boolean[] visited = new boolean[numberofvertex];
         boolean[] onstack = new boolean[numberofvertex];
         Stack<Integer> callstack = new Stack<>();
 
-        for(int point = 0; point < numberofvertex;point++){
-            int vertex = point;
-            if(visited[vertex])continue;
-            callstack.push(vertex);
+        for(int point = 0; point<numberofvertex;point++){
+            if(visited[point])continue;
+            callstack.add(point);
             while (!callstack.isEmpty()){
-                int currentVtx = callstack.peek();
-                if(!visited[currentVtx]){
-                    visited[currentVtx] = true;
-                    onstack[currentVtx]  = true;
-                }else {
-                    onstack[currentVtx] = false;
+                int currentVertex = callstack.peek();
+                if(!visited[currentVertex]){
+                    visited[currentVertex] = true;
+                    onstack[currentVertex] = true;
+                }else{
+                    onstack[currentVertex] = false;
+                    // poll extra
                     callstack.pop();
                     continue;
                 }
-                for(int nbedgeId = 0; nbedgeId<edgeListLength ;nbedgeId++){
-                    if(edgelist[nbedgeId][0]!=currentVtx)continue;
-                    int nbid = edgelist[nbedgeId][1];
-                    if(!visited[nbid]){
-                        callstack.push(nbid);
-                    }else if (onstack[nbid]){
-                        return true;
-                    }
+                // nbour navigate
+                for(int nbEdgeId = 0; nbEdgeId<edgeListLength; nbEdgeId++){
+                    if(edgelist[nbEdgeId][0]!=currentVertex)continue;
+                    int nbid = edgelist[nbEdgeId][1];// check single vertexes TODO
+                    if(!visited[nbid])callstack.push(nbid);
+                    else if(onstack[nbid])return true;
                 }
             }
-        }
 
+        }
         return false;
+
     }
 
 }
