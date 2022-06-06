a91d5260035d5f5608c68c6eed93f790fb2735eb
erguzel <erguzel@itu.edu.tr>
2022-06-06 19:24:38 +0200

    add remove tests

--- src/test/java/SampleTestActions.java
+++ src/test/java/SampleTestActions.java
@@ -1,100 +1,100 @@
-import lib.model.ProblemBase;
-import lib.util.ALogger;
-import org.junit.jupiter.api.*;
-
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.*;
-
-@Disabled
-class SampleTestActions extends ProblemBase {
-
-    static ALogger<SampleTestActions> LOGGER = new ALogger<>(SampleTestActions.class);
-    // needs to be STATIC
-    @BeforeAll
-    static void setup() {
-        LOGGER.info("@BeforeAll - executes once before all test methods in this class");
-    }
-
-    @BeforeEach
-    void init() {
-        LOGGER.info("@BeforeEach - executes before each test method in this class");
-    }
-
-    @AfterEach
-    void tearDown() {
-        LOGGER.info("@AfterEach - executed after each test method.");
-    }
-
-    @AfterAll
-    static void done() {
-        LOGGER.info("@AfterAll - executed after all test methods.");
-    }
-
-    @DisplayName("Single test successful")
-    @Test
-    void testSingleSuccessTest() {
-        LOGGER.info("Success");
-    }
-
-    @Test
-    @Disabled("Not implemented yet")
-    void testShowSomething() {
-    }
-
-    @Test
-    void lambdaExpressions() {
-        assertTrue(Stream.of(1, 2, 3)
-                .mapToInt(i -> i)
-                .sum() > 5, () -> "Sum should be greater than 5");
-    }
-
-    @Test
-    void groupAssertions() {
-        int[] numbers = {0, 1, 2, 3, 4};
-        assertAll("numbers",
-                () -> assertEquals(numbers[0], 0),
-                () -> assertEquals(numbers[3], 3),
-                () -> assertEquals(numbers[4], 4)
-        );
-    }
-
-    @Test
-    void trueAssumption() {
-        assumeTrue(5 > 1);
-        assertEquals(5 + 2, 7);
-    }
-
-    @Test
-    void falseAssumption() {
-        assumeFalse(5 < 1);
-        assertEquals(5 + 2, 7);
-    }
-
-    @Test
-    void assumptionThat() {
-        String someString = "Just a string";
-        assumingThat(
-                someString.equals("Just a string"),
-                () -> assertEquals(2 + 2, 4)
-        );
-    }
-
-    @Test
-    void shouldThrowException() {
-        Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
-            throw new UnsupportedOperationException("Not supported");
-        });
-        assertEquals(exception.getMessage(), "Not supported");
-    }
-
-    @Test
-    void assertThrowsException() {
-        String str = null;
-        assertThrows(IllegalArgumentException.class, () -> {
-            Integer.valueOf(str);
-        });
-    }
-
-}
+//import lib.model.ProblemBase;
+//import lib.util.ALogger;
+//import org.junit.jupiter.api.*;
+//
+//import java.util.stream.Stream;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assumptions.*;
+//
+//@Disabled
+//class SampleTestActions extends ProblemBase {
+//
+//    static ALogger<SampleTestActions> LOGGER = new ALogger<>(SampleTestActions.class);
+//    // needs to be STATIC
+//    @BeforeAll
+//    static void setup() {
+//        LOGGER.info("@BeforeAll - executes once before all test methods in this class");
+//    }
+//
+//    @BeforeEach
+//    void init() {
+//        LOGGER.info("@BeforeEach - executes before each test method in this class");
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//        LOGGER.info("@AfterEach - executed after each test method.");
+//    }
+//
+//    @AfterAll
+//    static void done() {
+//        LOGGER.info("@AfterAll - executed after all test methods.");
+//    }
+//
+//    @DisplayName("Single test successful")
+//    @Test
+//    void testSingleSuccessTest() {
+//        LOGGER.info("Success");
+//    }
+//
+//    @Test
+//    @Disabled("Not implemented yet")
+//    void testShowSomething() {
+//    }
+//
+//    @Test
+//    void lambdaExpressions() {
+//        assertTrue(Stream.of(1, 2, 3)
+//                .mapToInt(i -> i)
+//                .sum() > 5, () -> "Sum should be greater than 5");
+//    }
+//
+//    @Test
+//    void groupAssertions() {
+//        int[] numbers = {0, 1, 2, 3, 4};
+//        assertAll("numbers",
+//                () -> assertEquals(numbers[0], 0),
+//                () -> assertEquals(numbers[3], 3),
+//                () -> assertEquals(numbers[4], 4)
+//        );
+//    }
+//
+//    @Test
+//    void trueAssumption() {
+//        assumeTrue(5 > 1);
+//        assertEquals(5 + 2, 7);
+//    }
+//
+//    @Test
+//    void falseAssumption() {
+//        assumeFalse(5 < 1);
+//        assertEquals(5 + 2, 7);
+//    }
+//
+//    @Test
+//    void assumptionThat() {
+//        String someString = "Just a string";
+//        assumingThat(
+//                someString.equals("Just a string"),
+//                () -> assertEquals(2 + 2, 4)
+//        );
+//    }
+//
+//    @Test
+//    void shouldThrowException() {
+//        Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
+//            throw new UnsupportedOperationException("Not supported");
+//        });
+//        assertEquals(exception.getMessage(), "Not supported");
+//    }
+//
+//    @Test
+//    void assertThrowsException() {
+//        String str = null;
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            Integer.valueOf(str);
+//        });
+//    }
+//
+//}
--- src/test/java/TopologicalSortTest.java
+++ src/test/java/TopologicalSortTest.java
@@ -54,23 +54,22 @@ class TopologicalSortTest {
         };
         int numberofvertex1 = 7;
         int[] want1 = {0,3,1,2,4,6,5};
-        int [] res1 = instance.sortTopologicallyIfExists_Khan(input1,numberofvertex1, IGraph.GraphTypes.EDGELIST);
+        int [] res1 = instance.sortTopologicallyIfExists_Khan_ListyGraph(input1,numberofvertex1, IGraph.GraphTypes.EDGELIST);
 
         int[][] input2 = SampleData.Csacademy.COURSE_SCHEDULE_1;
         int numberofvertex2 = 6;
         int[] want2 = {5,4,3,2,0,1};
-        int[] res2 = instance.sortTopologicallyIfExists_Khan(input2,numberofvertex2,IGraph.GraphTypes.EDGELIST);
+        int[] res2 = instance.sortTopologicallyIfExists_Khan_ListyGraph(input2,numberofvertex2,IGraph.GraphTypes.EDGELIST);
 
         int[][] input3 = SampleData.Csacademy.COURSE_SCHEDULE_2;
         int numberofvertex3 = 6;
         int[] want3 = null;
-        int[] res3 =instance.sortTopologicallyIfExists_Khan(input3,numberofvertex3,IGraph.GraphTypes.EDGELIST);
+        int[] res3 =instance.sortTopologicallyIfExists_Khan_ListyGraph(input3,numberofvertex3,IGraph.GraphTypes.EDGELIST);
 
         int[][] input4 = SampleData.Csacademy.COURSE_SCHEDULE_3;
         int numberofvertex4 = 12;
         int[] want4 = {0,3,7,10,5,1,8,11,9,6,2,4};
-        int [] res4 = instance.sortTopologicallyIfExists_Khan(input4,numberofvertex4,IGraph.GraphTypes.EDGELIST);
-
+        int [] res4 = instance.sortTopologicallyIfExists_Khan_ListyGraph(input4,numberofvertex4,IGraph.GraphTypes.EDGELIST);
 
         assertArrayEquals(res1,want1,"test1");
         assertArrayEquals(res2,want2,"test2");
